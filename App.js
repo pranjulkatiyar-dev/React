42e39774288abf491fa38bfdeb914650bf0cc6cb
pranjul <pranjulkatiyar449@gmail.com>
2026-03-22 23:09:06 +0530

    Setup React with npm and updated files

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
-/**
- * <div id="parent">
- *    <div id="child">
- *        <h1> I'm h1 tag </h1>
- *        <h2> I'm h1 tag </h2>
- *    </div>
- *    <div id="child2">
- *        <h1> I'm h1 tag </h1>
- *        <h2> I'm h1 tag </h2>
- *    </div>
- * </div>
- * 
- */
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child"},
-        [ React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag"),
-]),
-    React.createElement("div",
-         {id:"child2"},
-        [  
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag"),])
-    ]);
-
-// const heading = React.createElement("h1", {id:"heading"}, 
-//     "Hello world from React!");
-
-    console.log(parent);
-    
+    React.createElement("div", {id:"child", key:"child1"}, [
+    React.createElement("h1", {key:"h1-1"}, "This is Namaste React"),
+    React.createElement("h2", {key:"h2-1"}, "I'm h2 tag"),
+    ]),
+    React.createElement("div", {id:"child2", key:"child2"}, [
+        React.createElement("h1", {key:"h1-2"}, "I'm h1 tag"),
+        React.createElement("h2", {key:"h2-2"}, "I'm h2 tag"),
+        React.createElement("h3", {key:"h3-2"}, "I'm h3 tag"),
+    ])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React
+
+
+#Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Cachingm - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Diagnostic
+- Error Handling
+- Tree Shaking - remove unused code
+- HTTPs
+- Different dev and produnction bundles
