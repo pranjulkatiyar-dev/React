cf5caa37e500a0b842a0259a148b0cd35f674056
pranjul <pranjulkatiyar449@gmail.com>
2026-04-18 15:10:33 +0530

    Restructured Project into src folder

---
+++ src/Components/Body.js
@@ -0,0 +1,21 @@
+import RestaurantCard from "./RestaurantCard";
+import reslist from "../utils/mockData";
+
+const Body = () =>{
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {reslist.map((restaurant) => (
+                                    <RestaurantCard 
+                        key={restaurant.info.id} 
+                        resData={restaurant} 
+                    />
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default Body;
